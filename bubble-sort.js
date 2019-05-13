db31ded32d4659cdb7b7d222180cc73821ae052d
zhengbo <zhengb@jiagouyun.com>
2019-05-13 10:03:58 +0800

    bubble sort

---
+++ bubble-sort.js
@@ -0,0 +1,50 @@
+let arr = [ 1, 2, 5, 8, 9, 10, 11, 12, 13 ]
+
+let swapCount = 0
+let count = 0
+
+
+
+function bubble_sort (arr) {
+  let length = arr.length
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - i; j++) {
+      count++
+      if (arr[j] > arr[j+1]) {
+        swapCount++
+        let tmp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = tmp
+      }
+    }
+  }
+}
+
+
+
+
+function bubble_sort_optimize (arr) {
+  let length = arr.length
+  for (let i = 0; i < length; i++) {
+    let flag = true
+    for (let j = 0; j < length - i; j++) {
+      count++
+      if (arr[j] > arr[j+1]) {
+        flag = false
+        swapCount++
+        let tmp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = tmp
+      }
+    }
+    // optimize 
+    if (flag) {
+      return
+    }
+  }
+}
+
+
+// bubble_sort(arr)
+bubble_sort_optimize(arr)
+console.log(arr, count, swapCount)
